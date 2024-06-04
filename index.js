a771053851fea21a9fa094dbbf69a40e3ed584e7
hammadsadi <hammad.sadi@yahoo.com>
2024-06-04 12:14:25 +0600

    Get Single Test Route Added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // Init Express
 const app = express();
 
@@ -54,6 +54,13 @@ async function run() {
     // Get All Tests
     app.get("/tests", async (req, res) => {
       const result = await testCollection.find().toArray();
+      res.send(result);
+    });
+    // Get Single Test
+    app.get("/tests/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await testCollection.findOne(query);
       res.send(result);
     });
     // await client.db("admin").command({ ping: 1 });
